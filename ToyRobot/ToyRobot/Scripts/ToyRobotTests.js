45be2a071bdb8ca8f64b1f423f4a44eb41335a0e
Taylor Piriyakulchai <36988527+corlepotae@users.noreply.github.com>
2018-12-16 15:35:02 +1100

    Add initial unit tests
    Add initial unit tests to test both valid and invalid inputs

--- ToyRobot/ToyRobot/Scripts/ToyRobotTests.js
+++ ToyRobot/ToyRobot/Scripts/ToyRobotTests.js
@@ -1,5 +1,21 @@
-﻿describe("ToyRobot Tests", function () {
-    it("Follows commands correctly", function () {
-        expect(false).toBeTruthy();
+﻿var ValidInputHandlingTests = [
+    { input: ['PLACE 0,0,NORTH', 'MOVE', 'REPORT'], expected: '0,1,NORTH' },
+    { input: ['PLACE 0,0,NORTH', 'LEFT', 'REPORT'], expected: '0,0,WEST' },
+    { input: ['PLACE 1,2,EAST', 'MOVE', 'MOVE', 'LEFT', 'MOVE', 'REPORT'], expected: '3,3,NORTH' }
+]
+var InvalidInputHandlingTests = [
+    { input: ['PLACE 0,0,NORTH', 'LEFT', 'MOVE', 'REPORT'], expected: '0,0,WEST' },
+]
+
+describe("ToyRobot Tests", function () {
+    it("Follows valid inputs correctly", function () {
+        for (var test of ValidInputHandlingTests) {
+            expect(false).toBeTruthy();
+        }
+    });
+    it("Handles invalid inputs correctly", function () {
+        for (var test of InvalidInputHandlingTests) {
+            expect(false).toBeTruthy();
+        }
     });
 });
