c322d368cfb17ea46784b19d559fcf4763d6ace8
Taylor Piriyakulchai <36988527+corlepotae@users.noreply.github.com>
2018-12-16 16:35:37 +1100

    Add robot turning function
    Add robot turning function and also add takeCommand function to take text command and translate into function execution

--- ToyRobot/ToyRobot/Scripts/ToyRobot.js
+++ ToyRobot/ToyRobot/Scripts/ToyRobot.js
@@ -7,6 +7,7 @@
             EAST: 1,
             SOUTH: 2,
             WEST: 3,
+            // A function for translating the enum value to string
             GetName: function (value) {
                 for (var item in this) {
                     if (this[item] === value)
@@ -17,9 +18,30 @@
     }
 
     constructor(maxX, maxY, x, y, f) {
-        this.maxDirection = Object.keys(Robot.Direction).length - 2;
+        // Set maximum and minimum directions the robot can use
         this.minDirection = Robot.Direction.NORTH;
+        this.maxDirection = Robot.Direction.WEST;
+
+        // Set surface area for the robot
+        this.setSurfaceArea(maxX, maxY);
+
+        // Place robot to a specific location
         this.place(x, y, f);
+    }
+
+    takeCommand(command) {
+        command = command.trim().toUpperCase();
+        switch (command) {
+            case "LEFT":
+                this.turnLeft();
+                break;
+            case "RIGHT":
+                this.turnRight();
+                break;
+            case "REPORT":
+                this.report();
+                break;
+        };
     }
 
     report() {
@@ -29,6 +51,44 @@
     place(x, y, f) {
         this.x = x;
         this.y = y;
-        this.f = f;
+        // Only set facing direction if the argument is a valid direction
+        if (f >= this.minDirection && f <= this.maxDirection) {
+            this.f = f;
+        }
+    }
+
+    turnLeft(turnCount = 1) {
+        this.turn(-turnCount);
+    }
+
+    turnRight(turnCount = 1) {
+        this.turn(turnCount); 
+    }
+
+    turn(turnCount = 1) {
+        // Calculating circular turning direction
+        var totalDirections = this.maxDirection + 1;
+        var nextDirection = (this.f + turnCount) % totalDirections;
+        // If next direction is less than minimum direction
+        // then do reverse calculation to find the next direction
+        if (nextDirection < this.minDirection) {
+            this.f = totalDirections - Math.abs(nextDirection);
+        }
+        // Otherwise assign the result directly
+        else {
+            this.f = nextDirection;
+        }
+    }	
+
+    setSurfaceArea(maxX, maxY) {
+        // Set surface area if the arguments are all numbers
+        if (!isNaN(maxX) && !isNaN(maxY)) {
+            // Set Surface area
+            this.surfaceArea =
+                {
+                    X: maxX,
+                    Y: maxY
+                };
+        }
     }
 }
--- ToyRobot/ToyRobot/Scripts/ToyRobotTests.js
+++ ToyRobot/ToyRobot/Scripts/ToyRobotTests.js
@@ -1,4 +1,15 @@
-﻿var ValidInputHandlingTests = [
+﻿var TurningTests = [
+    { input: ['RIGHT'], expected: 'EAST' },
+    { input: ['RIGHT', 'RIGHT'], expected: 'SOUTH' },
+    { input: ['RIGHT', 'RIGHT', 'RIGHT'], expected: 'WEST' },
+    { input: ['RIGHT', 'RIGHT', 'RIGHT', 'RIGHT'], expected: 'NORTH' },
+    { input: ['LEFT'], expected: 'WEST' },
+    { input: ['LEFT', 'LEFT'], expected: 'SOUTH' },
+    { input: ['LEFT', 'LEFT', 'LEFT'], expected: 'EAST' },
+    { input: ['LEFT', 'LEFT', 'LEFT', 'LEFT'], expected: 'NORTH' }
+]
+
+var ValidInputHandlingTests = [
     { input: ['PLACE 0,0,NORTH', 'MOVE', 'REPORT'], expected: '0,1,NORTH' },
     { input: ['PLACE 0,0,NORTH', 'LEFT', 'REPORT'], expected: '0,0,WEST' },
     { input: ['PLACE 1,2,EAST', 'MOVE', 'MOVE', 'LEFT', 'MOVE', 'REPORT'], expected: '3,3,NORTH' }
@@ -7,15 +18,24 @@ var InvalidInputHandlingTests = [
     { input: ['PLACE 0,0,NORTH', 'LEFT', 'MOVE', 'REPORT'], expected: '0,0,WEST' },
 ]
 
-describe("ToyRobot Tests", function () {
-    it("Follows valid inputs correctly", function () {
-        for (var test of ValidInputHandlingTests) {
-            expect(false).toBeTruthy();
+describe("ToyRobot should", function () {
+    it("turns correctly", function () {
+        for (var test of TurningTests) {
+            robot = new Robot(4, 4, 0, 0, Robot.Direction.NORTH);
+            for (var turn of test.input) {
+                robot.takeCommand(turn);
+            }
+            expect(Robot.Direction.GetName(robot.f)).toBe(test.expected);
         }
     });
-    it("Handles invalid inputs correctly", function () {
-        for (var test of InvalidInputHandlingTests) {
-            expect(false).toBeTruthy();
-        }
-    });
+    //it("follows valid inputs correctly", function () {
+    //    for (var test of ValidInputHandlingTests) {
+    //        expect(false).toBeTruthy();
+    //    }
+    //});
+    //it("handles invalid inputs correctly", function () {
+    //    for (var test of InvalidInputHandlingTests) {
+    //        expect(false).toBeTruthy();
+    //    }
+    //});
 });
